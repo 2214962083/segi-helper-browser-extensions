14c5e2014f7cb8e3e73954bfec0956d8e2fb6837
yangjinming <2214962083@qq.com>
2022-06-15 18:29:53 +0800

    feat: 添加 unocss 扩展推荐

--- components.d.ts
+++ components.d.ts
@@ -5,13 +5,10 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMenu: typeof import('element-plus/es')['ElMenu']
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
-    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
   }
 }
 
